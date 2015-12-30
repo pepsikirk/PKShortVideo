37bb6066b4d79d299162352f0432289a7ea99da9
pepsikirk <pepsikirk@pepsikirkdeimac.lan>
2015-12-31 00:53:10 +0800

    init

---
+++ PKShortVideoPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PKShortVideoPlayer
+//
+//  Created by pepsikirk on 15/12/30.
+//  Copyright © 2015年 pepsikirk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PKShortVideoPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PKShortVideoPlayer
+//
+//  Created by pepsikirk on 15/12/30.
+//  Copyright © 2015年 pepsikirk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
