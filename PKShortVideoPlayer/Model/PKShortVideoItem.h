185363984512afd37e8c17ab819f9a509361fa92
pepsikirk <pepsikirk@qq.com>
2016-01-03 23:54:57 +0800

    add model

---
+++ PKShortVideoPlayer/Model/PKShortVideoItem.h
@@ -0,0 +1,24 @@
+//
+//  PKShortVideoItem.h
+//  PKShortVideoPlayer
+//
+//  Created by pepsikirk on 16/1/3.
+//  Copyright © 2016年 pepsikirk. All rights reserved.
+//
+
+#import "JSQMediaItem.h"
+
+static CGFloat const PKShortVideoMaxLength = 220;
+
+
+@interface PKShortVideoItem : JSQMediaItem
+
+@property (nonatomic, strong) NSURL *fileURL;
+
+@property (strong, nonatomic) UIImage *image;
+
+- (void)play ;
+
+- (void)pause;
+
+@end
---
+++ PKShortVideoPlayer/Model/PKShortVideoItem.m
@@ -0,0 +1,21 @@
+//
+//  PKShortVideoItem.m
+//  PKShortVideoPlayer
+//
+//  Created by pepsikirk on 16/1/3.
+//  Copyright © 2016年 pepsikirk. All rights reserved.
+//
+
+#import "PKShortVideoItem.h"
+
+@implementation PKShortVideoItem
+
+- (void)play {
+    
+}
+
+- (void)pause {
+    
+}
+
+@end
