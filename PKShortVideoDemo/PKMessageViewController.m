06720dcbda87414ad30c94d76c1b1c24b6c2fb7b
蒋欣材 <pepsikirk@qq.com>
2026-09-16 09:38:11 +0800

    Remove obsolete demo playback branch

--- PKShortVideoDemo/PKMessageViewController.m
+++ PKShortVideoDemo/PKMessageViewController.m
@@ -485,14 +485,6 @@ static NSArray<NSString *> *PKDemoVideoPathsInDocuments(void) {
                                               NSUnderlineStyleAttributeName : @(NSUnderlineStyleSingle | NSUnderlinePatternSolid) };
     }
     
-    //iOS8以上才有willDisplayCell方法，判断iOS7在这里播放
-    if ([UIDevice currentDevice].systemVersion.floatValue <= 8.0) {
-        if ([msg.media isKindOfClass:[PKShortVideoItem class]]) {
-            PKShortVideoItem *item = (PKShortVideoItem *)msg.media;
-            [item play];
-        }
-    }
-    
     return cell;
 }
 
